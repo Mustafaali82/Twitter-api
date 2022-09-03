df5653b364cf5b4aa772fb81981e902faf9756bd
Mustafaali82 <67122730+Mustafaali82@users.noreply.github.com>
2022-09-03 12:12:05 +0500

    Twiiter API FIle Updated

---
+++ src/Components/User/User.js
@@ -0,0 +1,99 @@
+import React from "react";
+import classes from "./User.module.css";
+const User = () => {
+  return (
+    <div className={classes.userSec}>
+      <h1>Users</h1>
+      <div className={classes.box}>
+        <img
+          className={classes.profileImage}
+          src="jUTq7LRQ_400x400.jpg"
+          alt="Porfile_image"
+        />
+        <div className={classes.headingSec}>
+          <h3>Mustafa Ali</h3>
+          <span className={classes.span}>@TechgeekMustafa</span>
+        </div>
+      </div>
+      <div className={classes.box}>
+        <img
+          className={classes.profileImage}
+          src="jUTq7LRQ_400x400.jpg"
+          alt="Porfile_image"
+        />
+        <div className={classes.headingSec}>
+          <h3>Mustafa Ali</h3>
+          <span className={classes.span}>@TechgeekMustafa</span>
+        </div>
+      </div>
+      <div className={classes.box}>
+        <img
+          className={classes.profileImage}
+          src="jUTq7LRQ_400x400.jpg"
+          alt="Porfile_image"
+        />
+        <div className={classes.headingSec}>
+          <h3>Mustafa Ali</h3>
+          <span className={classes.span}>@TechgeekMustafa</span>
+        </div>
+      </div>
+      <div className={classes.box}>
+        <img
+          className={classes.profileImage}
+          src="jUTq7LRQ_400x400.jpg"
+          alt="Porfile_image"
+        />
+        <div className={classes.headingSec}>
+          <h3>Mustafa Ali</h3>
+          <span className={classes.span}>@TechgeekMustafa</span>
+        </div>
+      </div>
+      <div className={classes.box}>
+        <img
+          className={classes.profileImage}
+          src="jUTq7LRQ_400x400.jpg"
+          alt="Porfile_image"
+        />
+        <div className={classes.headingSec}>
+          <h3>Mustafa Ali</h3>
+          <span className={classes.span}>@TechgeekMustafa</span>
+        </div>
+      </div>
+      <div className={classes.box}>
+        <img
+          className={classes.profileImage}
+          src="jUTq7LRQ_400x400.jpg"
+          alt="Porfile_image"
+        />
+        <div className={classes.headingSec}>
+          <h3>Mustafa Ali</h3>
+          <span className={classes.span}>@TechgeekMustafa</span>
+        </div>
+      </div>
+      <div className={classes.box}>
+        <img
+          className={classes.profileImage}
+          src="jUTq7LRQ_400x400.jpg"
+          alt="Porfile_image"
+        />
+        <div className={classes.headingSec}>
+          <h3>Mustafa Ali</h3>
+          <span className={classes.span}>@TechgeekMustafa</span>
+        </div>
+      </div>
+      <div className={classes.box}>
+        <img
+          className={classes.profileImage}
+          src="jUTq7LRQ_400x400.jpg"
+          alt="Porfile_image"
+        />
+        <div className={classes.headingSec}>
+          <h3>Mustafa Ali</h3>
+          <span className={classes.span}>@TechgeekMustafa</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default User;
